19b668383efab66ce385fe86271a3177eb479381
takuro <talangxiaotian6@gmail.com>
2025-03-14 18:07:20 +0900

    ボタン画面の作成

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Button = () => {
-  return <div>Button</div>;
+  const buttonStyle = {
+    width: "80px",
+    height: "80px",
+    borderRadius: "50%",
+    borderStyle: "solid",
+    borderColor: "#fff",
+    outline: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+    cursor: "pointer",
+  };
+
+  const iconStyle = {
+    fontSize: "2.2rem",
+    color: "#fff",
+  };
+
+  return (
+    <button style={buttonStyle}>
+      <span style={iconStyle}>&#9654;</span>
+    </button>
+  );
 };
 
 export default Button;
