23dc385e33e035c210f9da645ae0af1e349f662f
takuro <talangxiaotian6@gmail.com>
2025-03-14 14:36:30 +0900

    TimerコンポーネントとButtonコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import "./App.css";
+import Button from "./Button";
+import Timer from "./Timer";
 
 function App() {
   return (
-    <div className="App">
-      Hello React
-    </div>
+    <>
+      <Timer />
+      <Button />
+    </>
   );
 }
 
